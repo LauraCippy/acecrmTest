bbb561d5312ac02fb4f1bf929139022716c6f7fd
LauraCippy <lauracomparelli912@gmail.com>
2025-04-18 10:20:44 +0200

    prova 2

--- libreria/config.inc.php
+++ libreria/config.inc.php
@@ -2,14 +2,14 @@
 
 define ("VERSION","1.0.1"); // aggiornare per far caricare automaticamente i javascript modificati
 
-define ("DBHOST","127.0.0.1");
+define ("DBHOST","95.110.131.98");
 /*locale_get_default
 define ("DBUSER","prova");
 define ("DBPASS","prova");
 define ("DBNAME","admin_zero");
 */
 
-define ("DBUSER","Shitan");
+define ("DBUSER","testAcecrm");
 define ("DBPASS","!Demetrio1977Adele");
 define ("DBNAME","");
 
